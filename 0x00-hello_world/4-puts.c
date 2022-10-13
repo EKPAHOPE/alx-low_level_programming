b43ee1924f1c62acd3de09072a4eaf8d7f958e9f
EKPAHOPE <EKPAHOPE>
2022-10-13 14:56:01 -0700

    Update 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * main - The entry point for the program
+ *
+ *   * Return: The exit code
+ *
+ *    */
+
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
