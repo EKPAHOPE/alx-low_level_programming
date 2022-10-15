fcde852dfdc3949e60b8ddfe57f8cf3708d203f8
EKPAHOPE <EKPAHOPE>
2022-10-14 18:11:30 -0700

    Create 4-print_alphabet.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  * main - The entry point for the program
+  * Return: The exit code
+  */
+int main(void)
+{
+	char ch = 'a';
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		char value = ch + i;
+
+		if (value == 'q' || value == 'e')
+			continue;
+		putchar(value);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
