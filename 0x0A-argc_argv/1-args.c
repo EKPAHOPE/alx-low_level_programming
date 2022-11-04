85cbdf6f4fb4c8cc3a805a81ccb900396c68ef88
EKPAHOPE <ekpahopejames@gmail.com>
2022-11-05 00:22:07 +0100

    Creat 1-args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,18 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - function that prints the number of arguments passed
- * @argc: argument count
- * @argv: charater array of passed arguments
- *
- * Return: 0
- */
+ * main - print number of arguments entered
+ * @argc: number of arguments
+ * @argv: arguments entered
+ * Description: print number of arguments
+ * Return: return zero
+ **/
 
-
-int main(int argc, char __attribute__((__unused__)) *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%d\n", argc - 1);
+	argv = argv;
+
 	return (0);
 }
