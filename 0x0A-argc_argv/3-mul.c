ba23b912b9bc8cbccab1ce53b86fddc85651f231
EKPAHOPE <ekpahopejames@gmail.com>
2022-11-04 16:56:23 -0700

    Create 3-mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,29 +1,39 @@
-#include main.h
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * main - multiplies two numbers
- * @argc: number of argument passed
- * @argv: array of arguments
- *
- * Return: returns nothing
- */
-
-int main(int __attribute__((__unused__)) argc, char *argv[])
+ * main - multiply two numbers
+ * @argc: number of arguments
+ * @argv: arguments entered
+ * Description: return product
+ * Return: zero
+ **/
+int main(int argc, char *argv[])
 {
-	int x, y;
+	int num1, num2;
+	char *s1 = argv[1];
+	char *s2 = argv[2];
 
-	if (argc <= 2)
+	num1 = num2 = 0;
+
+	if (argc != 3)
 	{
-		printf("%s\n", "Error");
+		printf("Error\n");
 		return (1);
 	}
 
+	if (s1[0] == '-')
+		num1 = -1 * atoi(&s1[1]);
+	else
+		num1 = atoi(&s1[0]);
 
-	x = atoi(argv[1]);
-	y = atoi(argv[2]);
+	if (s2[0] == '-')
+		num2 = -1 * atoi(&s2[1]);
+	else
+		num2 = atoi(&s2[0]);
 
-	printf("%d\n", x * y);
-
+	printf("%d\n", num1 * num2);
 	return (0);
 }
+
