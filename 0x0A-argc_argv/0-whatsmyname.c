c812635dd9ce7f4a95a87b8cb622ca5e6e15d348
EKPAHOPE <ekpahopejames@gmail.com>
2022-11-05 00:04:34 +0100

    My name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+
 /**
-* main -  writes program name
-* @argc: arguement count
-* @argv: array of pointers to CLI arguement
-* Return: 0
-**/
+ * main - prints name of file
+ * @argc: number of arguments
+ * @argv: arguments entered
+ * Description: print name of file
+ * Return: zero
+ **/
+
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[argc - 1]);
+	printf("%s\n", argv[0]);
+	argc = argc;
+
 	return (0);
 }
